5e2530adcaa9ecbe734fa812160d63230ce26c6e
OIKAWA Yuki <153487304+5unad0ke1@users.noreply.github.com>
2026-06-16 10:23:40 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://links.sunadokei.dev/',
+  site: 'https://5unad0ke1.github.io/links-site/',
   base: '/',
   trailingSlash: 'always',
   output: 'static',
